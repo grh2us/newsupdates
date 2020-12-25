d9db03710e83d29da0e71a688d7da778dbaf5d16
Gopal Hari <gopalhari@Gopals-MBP.fios-router.home>
2020-12-25 17:51:13 -0500

    Intial project files

---
+++ src/main/java/com/realtime/newsupdates/NewsupdatesApplication.java
@@ -0,0 +1,13 @@
+package com.realtime.newsupdates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsupdatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsupdatesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/realtime/newsupdates/NewsupdatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realtime.newsupdates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsupdatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
