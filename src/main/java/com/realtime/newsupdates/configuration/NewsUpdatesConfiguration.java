9ac4763da335ce8d7db8ff3020ebe658c69c5d4a
Gopal Hari <gopalhari@Gopals-MBP.fios-router.home>
2020-12-26 15:46:14 -0500

    Polling jokes api and publishing data to Kafka

--- src/main/java/com/realtime/newsupdates/configuration/NewsUpdatesConfiguration.java
+++ src/main/java/com/realtime/newsupdates/configuration/NewsUpdatesConfiguration.java
@@ -1,15 +1,24 @@
 package com.realtime.newsupdates.configuration;
 
 import com.realtime.newsupdates.model.Joke;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Sinks;
+import reactor.kafka.sender.KafkaSender;
+import reactor.kafka.sender.SenderOptions;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class NewsUpdatesConfiguration {
     private static final String JOKE_API_ENDPOINT = "https://official-joke-api.appspot.com/jokes/random";
+    private static final String BOOTSTRAP_SERVERS = "localhost:9092";
+    private static final String CLIENT_ID_CONFIG = "newsupdate_producer_1";
 
     @Bean
     public WebClient webClient(){
@@ -18,13 +27,27 @@ public class NewsUpdatesConfiguration {
                 .build();
     }
 
-    @Bean
-    public Sinks.Many<Joke> sink(){
-        return Sinks.many().replay().latest();
-    }
+//    @Bean
+//    public Sinks.Many<Joke> sink(){
+//        return Sinks.many().replay().latest();
+//    }
+//
+//    @Bean
+//    public Flux<Joke> flux(Sinks.Many<Joke> sink){
+//        return sink.asFlux();
+//    }
 
     @Bean
-    public Flux<Joke> flux(Sinks.Many<Joke> sink){
-        return sink.asFlux();
+    public KafkaSender<String, String> kafkaSender() {
+        Map<String, Object> props = new HashMap<>();
+
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, CLIENT_ID_CONFIG);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        SenderOptions<String, String> senderOptions = SenderOptions.create(props);
+
+        return KafkaSender.create(senderOptions);
     }
 }
--- src/main/java/com/realtime/newsupdates/service/NewsPublisher.java
+++ src/main/java/com/realtime/newsupdates/service/NewsPublisher.java
@@ -2,27 +2,40 @@ package com.realtime.newsupdates.service;
 
 import com.realtime.newsupdates.model.Joke;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Sinks;
+import reactor.core.publisher.Flux;
+import reactor.kafka.sender.KafkaSender;
+import reactor.kafka.sender.SenderRecord;
 
 @Service
+@Slf4j
 public class NewsPublisher {
     @Autowired
     private WebClient webClient;
 
     @Autowired
-    private Sinks.Many<Joke> sink;
+    private KafkaSender<String, String> kafkaSender;
+
+    private static final String TOPIC = "news-topic";
 
     @Scheduled(fixedRate = 3000)
-    public void publish(){
-        System.out.println("Helloooooooooo!");
-        this.webClient
+    public void publish() {
+        log.info("Publishing more data");
+        dispatchMessage(this.webClient
                 .get()
                 .retrieve()
-                .bodyToMono(Joke.class)
-                .subscribe(this.sink::tryEmitNext);
+                .bodyToFlux(Joke.class)
+        );
+    }
+
+    public void dispatchMessage(Flux<Joke> jokeFlux) {
+        kafkaSender.send(jokeFlux
+                .map(i -> SenderRecord.create(new ProducerRecord<>(TOPIC, i.toString()), i)))
+                .doOnError(e -> log.error("Send failed", e))
+                .subscribe(r -> log.info("Sent to topic {}", r.recordMetadata().topic()));
     }
 }
